95aa1a9191ebf81b36fa9a187bc9e0dd7a930c5b
ekaterinaey <ekaterina40990@rambler.ru>
2022-08-05 20:05:59 +0300

    Ex002

---
+++ Ex002/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите ваше имя"); // Вывод текста в терминал Console.Write без переноса на новую строку
+string username = Console.ReadLine(); // Ввод данных
+Console.Write("Привет,");
+Console.Write(username);
+
+// Вывод текста в терминал Console.WriteLine с переносом строки
+
